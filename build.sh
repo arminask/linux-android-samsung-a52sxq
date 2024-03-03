d4314f524ca880bde66a6a1d6df5c8e30c0fd04f
arminask <arminas@ver.lt>
2024-03-03 18:50:00 +0000

    added .gitignore

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ TC_DIR=/home/arminas/test/clangg
 JOBS=$(nproc)
 MAKE_PARAMS="-j$JOBS -C $SRC_DIR O=$SRC_DIR/out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 CROSS_COMPILE=$TC_DIR/bin/llvm-"
 export PATH="$TC_DIR/bin:$PATH"
-make $MAKE_PARAMS vendor/a52sxq_eur_open_defconfig
+make $MAKE_PARAMS droidian-defconfig
 make $MAKE_PARAMS
